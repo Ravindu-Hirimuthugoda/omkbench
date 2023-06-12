ae4c8215695e6fd743e72bb64fedc7d7d6de8019
Ravindu-Hirimuthugoda <ravindu.18@cse.mrt.ac.lk>
2023-06-12 12:02:19 +0530

    graphs

--- python/plot.py
+++ python/plot.py
@@ -3,6 +3,7 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 def read(fname: str) -> np.array:
@@ -63,11 +64,11 @@ def plot_variants(A: np.array, title: str) -> None:
         fig = plt.figure(figsize=(9, 9))
         B = A[A[:, 0] == kernel]
 
-        block_size = np.unique(B[:, 1].astype(int))
+        block_size = np.unique(B[:, 2].astype(int))
         for bsize in block_size:
-            C = B[B[:, 1].astype(int) == bsize]
-            words = C[:, 2].astype(int)
-            time = C[:, 3].astype(float)
+            C = B[B[:, 2].astype(int) == bsize]
+            words = C[:, 3].astype(int)
+            time = C[:, 4].astype(float)
             plt.plot(words, time, label=f"block-size={bsize}")
 
         plt.title(f"{title} Time: {kernel}", fontsize=23)
@@ -81,16 +82,127 @@ def plot_variants(A: np.array, title: str) -> None:
         plt.savefig(f"{kernel}.pdf")
         plt.close()
 
+def plot_v(A: np.array, title: str) -> None:
+    kernels = np.unique(A[:, 0])
+
+    for kernel in kernels:
+        fig = plt.figure(figsize=(9, 9))
+        B = A[A[:, 0] == kernel]
+
+        block_size = np.unique(B[:, 1])
+        for bsize in block_size:
+            C = B[B[:, 1] == bsize]
+            words = C[:, 2].astype(int)
+            time = C[:, 3].astype(float)
+            plt.plot(words, time, label=f"backend={bsize}")
+
+        plt.title(f"{title} Time: {kernel}", fontsize=23)
+        plt.xlabel("# of 8-byte Words", fontsize=18)
+        plt.ylabel("Time (s)", fontsize=18)
+        plt.xscale("log", base=10)
+        plt.yscale("log", base=10)
+        plt.xticks(fontsize=16)
+        plt.yticks(fontsize=16)
+        plt.legend(loc=0)
+        plt.savefig(f"{title}.pdf")
+        plt.close()
+
+def plot_bnd(A: np.array, title: str) -> None:
+    kernels = np.unique(A[:, 0])
+
+    for kernel in kernels:
+        fig = plt.figure(figsize=(9, 9))
+        B = A[A[:, 0] == kernel]
+
+        block_size = np.unique(B[:, 1])
+        for bsize in block_size:
+            C = B[B[:, 1] == bsize]
+            words = C[:, 3].astype(int)
+            time = C[:, 4].astype(float)
+            knl_time = C[:, 5].astype(float)
+            precentage = (time / knl_time)*100
+            plt.plot(words, precentage, label=f"backend={bsize}")
+
+        plt.title(f"{title} Knl_bnd : bnd {kernel}", fontsize=23)
+        plt.xlabel("Words", fontsize=18)
+        plt.ylabel("Time (s)", fontsize=18)
+        plt.xscale("log", base=10)
+        plt.yscale("log", base=10)
+        plt.xticks(fontsize=16)
+        plt.yticks(fontsize=16)
+        plt.legend(loc=0)
+        plt.savefig(f"vec-sclr-bnd.pdf")
+        plt.close()
+
+def plot_flop(A: np.array, title: str) -> None:
+    kernels = np.unique(A[:, 0])
+
+    # f = open("demofile3.txt", "a")
+    for kernel in kernels:
+        fig = plt.figure(figsize=(9, 9))
+        B = A[A[:, 0] == kernel]
+        back_end = np.unique(B[:, 1])
+        for bnd in back_end:
+            # print(bnd)
+            C = B[B[:, 1] == bnd]
+            words = C[:, 2].astype(int)
+            time = C[:, 3].astype(float)
+            # flps = words / time
+            # ai = (2 * words * 8)
+            flps = (words + np.log2(words)) / time
+            ai = flps / (words * 8)
+            plt.plot(ai, flps, label=f"backend={bnd}")
+
+        plt.title(f"{title} Time: {kernel}", fontsize=23)
+        plt.xlabel("AI(Flops:Byte)", fontsize=18)
+        plt.ylabel("Flops", fontsize=18)
+        plt.xscale("log", base=10)
+        plt.yscale("log", base=10)
+        plt.xticks(fontsize=16)
+        plt.yticks(fontsize=16)
+        plt.legend(loc=0)
+        plt.savefig(f"try-ai-with-words.pdf")
+        plt.close()
+
+
+
 
 if __name__ == "__main__":
-    A = read("data/frontier/omk_h2d_d2h.txt")
-    plot_h2d_d2h(A)
+    # A = read("data/frontier/omk_h2d_d2h.txt")
+    # plot_h2d_d2h(A)
 
-    B = read("data/frontier/omk_d2d.txt")
-    plot_d2d(B)
+    # B = read("data/frontier/omk_d2d.txt")
+    # plot_d2d(B)
 
-    C = read("data/frontier/omk_daxpy.txt")
-    plot_variants(C, "Daxpy")
+    # C = read("data/frontier/omk_daxpy.txt")
+    # plot_variants(C, "Daxpy")
 
-    D = read("data/frontier/omk_reduction.txt")
-    plot_variants(C, "Reduction")
+    # D = read("data/frontier/omk_reduction.txt")
+    # plot_variants(D, "Reduction")
+
+    # E = read("data/frontier/lanczos.txt")
+    # plot_flop(E, "Lanczos")
+
+    # F = read("data/frontier/lanczos_vec-norm.txt")
+    # plot_flop(F, "vec-norm-roof")
+
+    # G = read("data/frontier/lanczos_vec-sclr-div.txt")
+    # plot_flop(G, "vec-sclr-div1")
+
+    # H = read("data/frontier/lanczos_mtx-col-copy.txt")
+    # plot_v(H, "mtx-col-copy")
+
+    # I = read("data/frontier/lanczos_calc-w.txt")
+    # plot_flop(I, "calc-w")
+    
+
+    # flops = (990814+ np.log2(990814)) / 1.205043e-03
+    # ai = flops / (990814 * 8)
+    # print(flops)
+    # print(ai)
+
+    # I = read("data/frontier/lanczos_vec-sclr-div-lsize.txt")
+    # plot_variants(I, "vec-sclr-div-lsize")
+
+    J = read("data/frontier/lanczos_vec-sclr-div-bnd.txt")
+    plot_bnd(J, "vec_sclr_div_bnd")
